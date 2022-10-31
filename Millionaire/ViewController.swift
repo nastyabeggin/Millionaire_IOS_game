aa9a095b68615b833556c03d514d2fb646173d8c
mr-kelma <mr.kelma@gmail.com>
2022-10-31 12:43:50 +0400

    Initial Commit

---
+++ Millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Valery Keplin on 31.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
