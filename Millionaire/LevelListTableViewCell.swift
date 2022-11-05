b37f273d81543f1f1fe1fb3f14502ce3627490d6
nastyabeggin <45201186+nastyabeggin@users.noreply.github.com>
2022-11-05 20:24:22 +0300

    changed color of permanent sums

--- Millionaire/LevelListTableViewCell.swift
+++ Millionaire/LevelListTableViewCell.swift
@@ -28,7 +28,10 @@ class LevelListTableViewCell: UITableViewCell {
             titleLabel.text = question.number
             if let answer = question.isAnswered {
                 titleLabel.backgroundColor = answer ? .systemGreen : .systemRed
-            } else {
+            } else if question.number.contains("⭐️") || question.number.contains("🌟"){
+                titleLabel.backgroundColor = .blue
+                titleLabel.textColor = .white
+            }else {
                 titleLabel.backgroundColor = .systemGray6
             }
         }
